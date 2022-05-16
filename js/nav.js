dd7c8c8daf5e9aca83dca8cbef3760642f40e9c2
Nati Ba <nzbartolome@gmail.com>
2022-05-16 00:35:43 -0300

    nav js 6th commit

---
+++ js/nav.js
@@ -0,0 +1,48 @@
+// Navigation Desktop Insert
+
+document.getElementById("navig-wrapper").innerHTML = `
+<div class="desk-nav-container-flex">
+<div class="logo-desktop">
+<a href="/index.html"> <img src="https://media-prd.coachella.com/templates/template10/svgs/coachella-logo.svg" alt="logo"></a>
+<div class="mob-line">
+<p class="fly">APR 15-17 & 22-24</p>
+</div>
+</div>
+  <div class="c-navigation__container">
+  <div class="c-navigation">
+      <div class="c-desk-navigation-navList">
+
+          <ul class="c-navigation__navList" id="nav-list">
+              <li class="c-navigation__navItem">
+                  <a class="c-title" href="#"
+                      target="_self">Music</a>
+              </li>
+              <li class="c-navigation__navItem" data-identifier="navitem2866">
+                  <a class="c-title" href="#"
+                      target="_self">Passes</a>
+              </li>
+              <li class="c-navigation__navItem" data-identifier="navitem2867">
+                  <a class="c-title" href="#"
+                      target="_self">Plan</a>
+              </li>
+              <li class="c-navigation__navItem" data-identifier="navitem2868">
+                  <a class="c-title" href="#"
+                      target="_self">Experience</a>
+              </li>
+              <li class="c-navigation__navItem" data-identifier="navitem2955">
+                  <a class="c-title" href="#"
+                      target="_self">Shop</a>
+              </li>
+              <li class="c-navigation__navItem" data-identifier="navitem2955">
+                  <a class="c-title" href="/html/contact.html"
+                      target="_self">Contact</a>
+              </li>
+              <li class="search-btn">
+                  <img alt="" src="https://media-prd.coachella.com/templates/template10/svgs/search.svg" />
+              </li>
+          </ul>
+          </div>
+          </div>
+          </div> 
+          </div>
+  `;

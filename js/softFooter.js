3cc0ebadbb310dbbb27fdda10689f3bf99f2a8e0
Nati Ba <nzbartolome@gmail.com>
2022-05-17 21:25:39 -0300

    learnMore

---
+++ js/softFooter.js
@@ -0,0 +1,52 @@
+// Soft Footer Insert
+
+let softFooter = document.getElementById('soft-footer').innerHTML = `
+<div class="footer-logo">
+      <a href="/index.html"> <img class="footer-logo-collection" src="https://media-prd.coachella.com/templates/template10/svgs/coachella-logo.svg" alt="logo"></a>
+      <p>&copy 2022 Sound&Music</p>
+    </div>
+    <section class="politics politics-media">
+      <article>
+        <p>Privacy Policy</p>
+        <p>Terms of Use</p>
+        <p>Wristband Terms of Use</p>
+        <p>Accessibility (ADA)</p>
+      </article>
+      <article>
+        <p>Do Not Sell My Info</p>
+        <p>Web Accessibility</p>
+        <p>Sponsors</p>
+        <p>Contact</p>
+      </article>
+    </section>
+    <div class="btnAndSocial">
+    <a class="nwl wks" href="/html/newsletter.html">Get the Newsletter</a>
+    <article class="social">
+      <ul>
+        <li>
+          <a href="https://www.facebook.com/coachella"
+            ><i class="fa fa-facebook-official w3-hover-opacity"></i
+          ></a>
+        </li>
+        <li>
+          <a href="https://twitter.com/coachella"
+            ><i class="fa fa-twitter w3-hover-opacity"></i
+          ></a>
+        </li>
+        <li>
+          <a href=""><i class="fa fa-instagram w3-hover-opacity"></i></a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/user/coachella"
+            ><i class="fa fa-brands fa-youtube"></i
+          ></a>
+        </li>
+        <li>
+          <a href="https://www.snapchat.com/add/coachella"
+            ><i class="fa fa-brands fa-snapchat"></i
+          ></a>
+        </li>
+      </ul>
+    </article>
+  </div>
+`
